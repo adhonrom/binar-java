c6f00b9df61a493ad96f89241eefc79553c44f2e
Adhonrom <adhonrom@gmail.com>
2018-10-24 20:22:28 +0700

    pertemuan kedua

---
+++ pertemuanKedua/src/HelloWorld.java
@@ -0,0 +1,25 @@
+//
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("binar\nacademy ");
+        System.out.print("bsd");
+        System.out.println();
+        newMain();
+        System.out.println(15);
+        mainLagi();
+    }
+
+    public static void newMain() {
+        System.out.println("coba apa lagi coba");
+    }
+
+    public static void mainLagi() {
+        int angka = 5;
+        System.out.println(angka * 4);
+    }
+}
---
+++ pertemuanKedua/src/mengnalTipeData.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class mengnalTipeData {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("hi");
+        System.out.print("what your name");
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
+
+    }
+
+    public static void hitung () {
+
+        int x = 8;
+        int y = 2;
+
+        System.out.println("x + y " + (x + y));
+        System.out.println("x - y " + (x - y));
+
+        x--;
+
+
+        System.out.println("x -- " + x );
+
+
+    }
+
+}
+
+
+
+
+
