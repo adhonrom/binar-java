b084dfb1e0eb1f8a77fad021d741401a5c2175e7
Adhonrom <adhonrom@gmail.com>
2018-10-24 20:03:22 +0700

    coba kedua

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,33 @@
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
+import javax.sound.midi.Soundbank;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        /*
+        coding pertama kali
+         */
+
+        System.out.println("Hello World"); // print line (pindah baris)
+        System.out.print("binar\nacademy ");
+        System.out.print("bsd");
+        System.out.println();
+        newMain();
+        System.out.println(5 * 3);
+        mainLagi();
+
+    }
+
+
+    public static void newMain(){
+        System.out.println("coba apa lagi coba");
+    }
+
+    public static void mainLagi() {
+        int angka = 5;
+                System.out.println(angka * 4);
+
+    }
+
+}
